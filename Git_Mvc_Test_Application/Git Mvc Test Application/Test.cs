03b29fb8ce8098b974ca9d935f7e423d67f6e530
Igor Marchenko <gurupatik@hotmail.com>
2013-06-11 17:01:39 +0300

    second commit for update

---
+++ Git_Mvc_Test_Application/Git Mvc Test Application/Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Git_Mvc_Test_Application
+{
+    public class Test
+    {
+        public int TestProperty { get; set; }
+
+        public int TestMethod()
+        {
+            return 0;        
+        }
+    }
+}
